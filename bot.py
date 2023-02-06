ca6e744ce0b812dbda588c43a78adb1d652cd7e6
Evence Wang <shjp20070426@gmail.com>
2023-02-05 22:15:47 -0500

    new changes

--- bot.py
+++ bot.py
@@ -1,35 +1,80 @@
 import os
 import discord
-import responses
-
-
-async def send_message(message, user_message, is_private):
-  try:
-    response = responses.handle_response(user_message)
-    await message.author.send(response) if is_private else await message.channel.send(response)
-  except Exception as e:
-    print(e)
+from discord.ext import commands
+from ytdl import *
 
 
 def run_bot():
   TOKEN = os.environ['DISCORD_TOKEN']
-  client = discord.Client(intents=discord.Intents.all())
+  intents = discord.Intents.all()
+  intents.message_content = True
+  intents.voice_states = True
+  bot = commands.Bot(command_prefix='!', intents=intents)
 
-  @client.event
+  @bot.event
   async def on_ready():
-    print('{} is now running!'.format(client.user))
+    print('{} is now running!'.format(bot.user))
 
-  @client.event
-  async def on_message(message):
-    if message.author == client.user:
+  @bot.command(name='join', help='Bot joins the voice channel')
+  async def join(ctx: commands.Context):
+    '''Bot joins a voice channel'''
+    if not ctx.message.author.voice:
+      await ctx.send('{} is not connected to a voice channel, bot is unable to join.'\
+        .format(ctx.message.author.name))
       return
 
-    user_message = str(message.content)
+    channel = ctx.message.author.voice.channel
+    await channel.connect()
 
-    if user_message[0] == '?':
-      user_message = user_message[1:]
-      await send_message(message, user_message, is_private=True)
-    else:
-      await send_message(message, user_message, is_private=False)
+  @bot.command(name='leave', help='Bot leaves the voice channel')
+  async def leave(ctx: commands.Context):
+    voice_client = ctx.message.guild.voice_client
+    if voice_client.is_connected():
+      await stop(ctx)
+      await voice_client.disconnect()
+      return
+    await ctx.send('Bot is not connected to a voice channel.')
 
-  client.run(TOKEN)
+  @bot.command(name='play', help='Bot plays the audio from the given url')
+  async def play(ctx: commands.Context, search: str):
+    voice_channel = ctx.message.guild.voice_client
+    if not (voice_channel and voice_channel.is_connected()):
+      await ctx.invoke(join)
+      voice_channel = ctx.message.guild.voice_client
+
+    async with ctx.typing():
+      try:
+        source = await YTDLSource.create_source(ctx, search, loop=bot.loop)
+      except YTDLError as e:
+        await ctx.send('An error occurred while processing this request: {}'.format(str(e)))
+      else:
+        voice_channel.play(source)
+      await ctx.send('**Now playing:** {}'.format(source))
+
+  @bot.command(name='pause', help='Bot pauses the audio')
+  async def pause(ctx: commands.Context):
+    voice_client = ctx.message.guild.voice_client
+    if voice_client and voice_client.is_playing():
+      voice_client.pause()
+      return
+    await ctx.send('Bot is not playing anything.')
+
+  @bot.command(name='resume', help='Bot resumes the audio')
+  async def resume(ctx: commands.Context):
+    voice_client = ctx.message.guild.voice_client
+    if voice_client and voice_client.is_paused():
+      voice_client.resume()
+      return
+    await ctx.send('Bot is not paused, use **play** command to play something.')
+
+  @bot.command(name='stop', help='Bot stops the audio')
+  async def stop(ctx: commands.Context):
+    voice_client = ctx.message.guild.voice_client
+    if voice_client and voice_client.is_playing():
+      voice_client.stop()
+      await ctx.send('Stopped playing audio.')
+      voice_client.cleanup()
+      return
+    await ctx.send('Bot is not playing anything.')
+
+  bot.run(TOKEN)
---
+++ ytdl.py
@@ -0,0 +1,127 @@
+import asyncio
+import functools
+import youtube_dl
+import discord
+from discord.ext import commands
+
+
+youtube_dl.utils.bug_reports_message = lambda: ''
+
+
+class YTDLError(Exception):
+  pass
+
+class VoiceError(Exception):
+  pass
+
+
+class YTDLSource(discord.PCMVolumeTransformer):
+  ytdl_format_options = {
+    'format': 'bestaudio/best',
+    'extractaudio': True,
+    'audioformat': 'mp3',
+    'outtmpl': '%(extractor)s-%(id)s-%(title)s.%(ext)s',
+    'restrictfilenames': True,
+    'noplaylist': True,
+    'nocheckcertificate': True,
+    'ignoreerrors': False,
+    'logtostderr': False,
+    'quiet': True,
+    'no_warnings': True,
+    'default_search': 'auto',
+    'source_address': '0.0.0.0'
+  }
+
+  ffmpeg_options = {
+    'before_options': '-reconnect 1 -reconnect_streamed 1 -reconnect_delay_max 5',
+    'options': '-vn'
+  }
+
+  YTDL = youtube_dl.YoutubeDL(ytdl_format_options)
+
+  def __init__(self, ctx: commands.Context, source: discord.FFmpegPCMAudio, *, data: dict, volume: float = 0.5):
+    super().__init__(source, volume)
+
+    self.requester = ctx.author
+    self.channel = ctx.channel
+    self.data = data
+
+    self.uploader = data.get('uploader')
+    self.uploader_url = data.get('uploader_url')
+    date = data.get('upload_date')
+    self.upload_date = date[6:8] + '.' + date[4:6] + '.' + date[0:4]
+    self.title = data.get('title')
+    self.thumbnail = data.get('thumbnail')
+    self.description = data.get('description')
+    self.duration = self.parse_duration(int(data.get('duration')))
+    self.tags = data.get('tags')
+    self.url = data.get('webpage_url')
+    self.views = data.get('view_count')
+    self.likes = data.get('like_count')
+    self.dislikes = data.get('dislike_count')
+    self.stream_url = data.get('url')
+
+  def __str__(self):
+    return '**{0.title}** by **{0.uploader}**'.format(self)
+
+  @classmethod
+  async def create_source(cls, ctx: commands.Context, search: str, *, loop: asyncio.BaseEventLoop = None):
+    loop = loop or asyncio.get_event_loop()
+
+    partial = functools.partial(cls.YTDL.extract_info, search, download=False, process=False)
+    data = await loop.run_in_executor(None, partial)
+
+    if data is None:
+      raise YTDLError('Couldn not find anything that matches `{}`'.format(search))
+
+    if 'entries' not in data:
+      to_process = data
+    else:
+      to_process = None
+      for entry in data['entries']:
+        if entry:
+          to_process = entry
+          break
+
+      if to_process is None:
+        raise YTDLError('Could not find anything that matches `{}`'.format(search))
+
+    webpage_url = to_process['webpage_url']
+    partial = functools.partial(cls.YTDL.extract_info, webpage_url, download=False)
+    processed_info = await loop.run_in_executor(None, partial)
+
+    if processed_info is None:
+      raise YTDLError('Could not fetch `{}`'.format(webpage_url))
+
+    if 'entries' not in processed_info:
+      info = processed_info
+    else:
+      info = None
+      for entry in processed_info['entries']:
+        if entry:
+          info = entry
+          break
+
+      if info is None:
+        raise YTDLError('Could not retrieve any matches for `{}`'.format(webpage_url))
+
+    return cls(ctx, discord.FFmpegPCMAudio(info['url'], **cls.ffmpeg_options), data=info)
+
+  @staticmethod
+  def parse_duration(duration: int):
+    minutes, seconds = divmod(duration, 60)
+    hours, minutes = divmod(minutes, 60)
+    days, hours = divmod(hours, 24)
+
+    duration = []
+    if days > 0:
+      duration.append('{} days'.format(days))
+    if hours > 0:
+      duration.append('{} hours'.format(hours))
+    if minutes > 0:
+      duration.append('{} minutes'.format(minutes))
+    if seconds > 0:
+      duration.append('{} seconds'.format(seconds))
+
+    return ', '.join(duration)
+
